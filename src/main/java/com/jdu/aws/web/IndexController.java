fb840cefee27affb053d449aa3fbd051023b1345
mynameisdaun <daun9870.jung@gmail.com>
2021-11-17 18:51:33 +0900

    2021.11.17 JS

---
+++ src/main/java/com/jdu/aws/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.jdu.aws.web;
+
+import com.jdu.aws.web.dto.HelloResponseDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
