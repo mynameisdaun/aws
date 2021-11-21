11027517838cd2c8d13ae0d130502b91bd0eee02
mynameisdaun <daun9870.jung@gmail.com>
2021-11-21 23:36:16 +0900

    build stuck

--- src/main/java/com/jdu/aws/Application.java
+++ src/main/java/com/jdu/aws/Application.java
@@ -3,8 +3,6 @@ package com.jdu.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 //@EnableJpaAuditing
 @SpringBootApplication
